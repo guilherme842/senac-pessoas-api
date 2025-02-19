81df6c54811d07512fbb4bc838048d39b4fa611d
GUILHERME MARQUES MANGILLI <guilherme.mangilli@alunos.sc.senac.br>
2025-02-18 21:51:20 -0300

    aplicando novas tecnicas

--- senac-pedido-api/src/main/java/br/com/senac/pessoa/PessoasRepositorio.java
+++ senac-pedido-api/src/main/java/br/com/senac/pessoa/PessoasRepositorio.java
@@ -6,3 +6,24 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PessoasRepositorio extends JpaRepository<Pessoas, Long> {
 }
+
+@service
+public class PessoaService {
+
+    @Autowired
+    private PessoasRepositorio pessoasRepositorio;
+    /*
+    public  PessoasService(PessoasRepositorio pessoasRepositorio) {
+        this.pessoasRepositorio=pessoasRepositorio;
+    }
+    */
+    public Pessoas criarPessoa(Pessoas pessoa) {
+        try {
+            Pessoas pessoasResult = pessoasRepositorio.save(pessoa);
+
+            return pessoasResult;
+        } catch (Exception e) {
+            throws new RunTimeException(e.getMessage());
+        }
+    }
+}
